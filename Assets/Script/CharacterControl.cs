10b72b4176193940cdf90c11c85f9979f70661ad
HahaRiku <s9323839@gmail.com>
2018-10-05 11:05:33 +0800

    First Commit

---
+++ Assets/Script/CharacterControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControl : MonoBehaviour {
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            transform.localPosition = new Vector3(transform.localPosition.x + speed, transform.localPosition.y, transform.localPosition.z);
+            if (transform.localScale.x < 0) {
+                transform.localScale = new Vector3(transform.localScale.x * (-1), transform.localScale.y, transform.localScale.z);
+            }
+        }
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            transform.localPosition = new Vector3(transform.localPosition.x - speed, transform.localPosition.y, transform.localPosition.z);
+            if (transform.localScale.x > 0) {
+                transform.localScale = new Vector3(transform.localScale.x * (-1), transform.localScale.y, transform.localScale.z);
+            }
+        }
+    }
+}
