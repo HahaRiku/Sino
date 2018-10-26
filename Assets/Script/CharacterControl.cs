6a267d87cfc2bc566cb1c08f5e2b96695dbc9ca2
HahaRiku <s9323839@gmail.com>
2018-10-26 23:26:02 +0800

    修點animation+角色移動+背景測試

--- Assets/Script/CharacterControl.cs
+++ Assets/Script/CharacterControl.cs
@@ -4,25 +4,38 @@ using UnityEngine;
 
 public class CharacterControl : MonoBehaviour {
     public float speed;
+    private Animator ani;
 
 	// Use this for initialization
 	void Start () {
-
+        ani = gameObject.GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (Input.GetKey(KeyCode.RightArrow)) {
             transform.localPosition = new Vector3(transform.localPosition.x + speed, transform.localPosition.y, transform.localPosition.z);
-            if (transform.localScale.x < 0) {
+            if (transform.localScale.x > 0) {
                 transform.localScale = new Vector3(transform.localScale.x * (-1), transform.localScale.y, transform.localScale.z);
             }
         }
         if (Input.GetKey(KeyCode.LeftArrow)) {
             transform.localPosition = new Vector3(transform.localPosition.x - speed, transform.localPosition.y, transform.localPosition.z);
-            if (transform.localScale.x > 0) {
+            if (transform.localScale.x < 0) {
                 transform.localScale = new Vector3(transform.localScale.x * (-1), transform.localScale.y, transform.localScale.z);
             }
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow)) {
+            ani.SetBool("Walk", true);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            ani.SetBool("Walk", true);
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow)) {
+            ani.SetBool("Walk", false);
+        }
+        if (Input.GetKeyUp(KeyCode.LeftArrow)) {
+            ani.SetBool("Walk", false);
         }
     }
 }
