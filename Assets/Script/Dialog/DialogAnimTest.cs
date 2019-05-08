67faa47bdb2443f65517f5bcd49bb77a25aebd17
kopa0506 <kopa860506@gmail.com>
2019-05-08 17:14:52 +0800

    [更新] editor接口未完成 測試動畫上傳

---
+++ Assets/Script/Dialog/DialogAnimTest.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogAnimTest : MonoBehaviour
+{
+
+    public AnimationCurve animationCurve;
+    public CanvasGroup 對話框;
+    public CanvasGroup 人物;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Anim1()
+    {
+        StartCoroutine(anim1());
+    }
+
+    public void Anim2()
+    {
+        StartCoroutine(anim2());
+    }
+
+    IEnumerator anim1()
+    {
+        float timer = 0, orginY = 0.0f, finalY = -0.5f, totalTime = 0.5f, interval = 0.2f, y, alpha;
+        float waitBackTime = 1.0f;
+        while (timer < totalTime)
+        {
+            timer += Time.deltaTime;
+            y = animationCurve.Evaluate(timer / totalTime) * (finalY - orginY);
+            Camera.main.transform.position = new Vector3(0, orginY + y, -10);
+            if (timer > totalTime - interval)
+            {
+                alpha = animationCurve.Evaluate((timer - totalTime + interval) / interval);
+                對話框.alpha = alpha;
+                人物.alpha = alpha;
+            }
+            yield return new WaitForSeconds(Time.deltaTime);
+        }
+        Camera.main.transform.position = new Vector3(0, finalY, -10);
+        對話框.alpha = 1;
+        人物.alpha = 1;
+        yield return new WaitForSeconds(waitBackTime);
+        timer = 0;
+        while (timer < totalTime)
+        {
+            timer += Time.deltaTime;
+            y = animationCurve.Evaluate(timer / totalTime) * (finalY - orginY);
+            Camera.main.transform.position = new Vector3(0, finalY - y, -10);
+            if (timer < interval)
+            {
+                alpha = animationCurve.Evaluate(timer / interval);
+                對話框.alpha = 1 - alpha;
+                人物.alpha = 1 - alpha;
+            }
+            yield return new WaitForSeconds(Time.deltaTime);
+        }
+        Camera.main.transform.position = new Vector3(0, orginY, -10);
+        對話框.alpha = 0;
+        人物.alpha = 0;
+    }
+
+    IEnumerator anim2()
+    {
+        float timer = 0, orginY = 0.0f, finalY = -0.5f, totalTime = 0.5f, interval = 0.2f, y, alpha;
+        float waitBackTime = 1.0f;
+        while (timer < totalTime)
+        {
+            timer += Time.deltaTime;
+            y = animationCurve.Evaluate(timer / totalTime) * (finalY - orginY);
+            Camera.main.transform.position = new Vector3(0, orginY + y, -10);
+            Camera.main.orthographicSize = 5 - y;
+            if (timer > totalTime - interval)
+            {
+                alpha = animationCurve.Evaluate((timer - totalTime + interval) / interval);
+                對話框.alpha = alpha;
+                人物.alpha = alpha;
+            }
+            yield return new WaitForSeconds(Time.deltaTime);
+        }
+        Camera.main.transform.position = new Vector3(0, finalY, -10);
+        對話框.alpha = 1;
+        人物.alpha = 1;
+        yield return new WaitForSeconds(waitBackTime);
+        timer = 0;
+        while (timer < totalTime)
+        {
+            timer += Time.deltaTime;
+            y = animationCurve.Evaluate(timer / totalTime) * (finalY - orginY);
+            Camera.main.transform.position = new Vector3(0, finalY - y, -10);
+            Camera.main.orthographicSize = 5.5f + y;
+            if (timer < interval)
+            {
+                alpha = animationCurve.Evaluate(timer / interval);
+                對話框.alpha = 1 - alpha;
+                人物.alpha = 1 - alpha;
+            }
+            yield return new WaitForSeconds(Time.deltaTime);
+        }
+        Camera.main.transform.position = new Vector3(0, orginY, -10);
+        對話框.alpha = 0;
+        人物.alpha = 0;
+    }
+}
---
+++ Assets/Script/StorySystem/ScriptReaderWindow.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.IO;
+using System.Diagnostics;
+
+public class ScriptReaderWindow : EditorWindow {
+
+    string fileName;
+    string filePath;
+
+    ScriptReaderWindow()
+    {
+        titleContent = new GUIContent("Script Reader");
+    }
+
+    [MenuItem("Window/劇本讀入")]
+    static void Init()
+    {
+        ScriptReaderWindow window = (ScriptReaderWindow)GetWindow(typeof(ScriptReaderWindow));
+        window.Show();
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Space(10);
+        fileName = EditorGUILayout.TextField("劇本名稱", fileName);
+
+        //路徑選擇
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("劇本路徑  " + filePath);
+        GUILayout.FlexibleSpace();
+        if (GUILayout.Button("..."))
+            filePath = EditorUtility.OpenFilePanel("Overwrite with txt", "", "txt");
+        GUILayout.EndHorizontal();
+        GUI.skin.button.alignment = TextAnchor.MiddleCenter;
+        //確定
+        if (GUILayout.Button("按我生成 Story Data"))
+        {
+            if (File.Exists(filePath))
+            {
+                string dataAsTxt = File.ReadAllText(filePath);
+                UnityEngine.Debug.Log(dataAsTxt);
+            }
+            else
+            {
+                EditorUtility.DisplayDialog("帽の提醒", "發生錯誤：路徑不存在", "OK");
+            }
+        } 
+    }
+
+    private void LoadGameData()
+    {
+
+    }
+
+    /*private void SaveGameData()
+    {
+
+       // string dataAsJson = JsonUtility.ToJson(gameData);
+
+        string filePath = Application.dataPath + gameDataProjectFilePath;
+        //File.WriteAllText(filePath, dataAsJson);
+
+    }*/
+}
