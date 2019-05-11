e2c091849890f68c42143d0089ce2ce30eae8ae8
WanoonBrite <105502049@cc.ncu.edu.tw>
2019-05-11 18:45:20 +0800

    幾張地圖的行走修改(B3) + 部分的事件中動作

---
+++ Assets/Script/ActionController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using DragonBones;
+
+public class ActionController : MonoBehaviour {
+	private int speed;
+	private DragonBones.Animation ani;
+	
+	public IEnumerator WhatherWaiting_move(bool waiting, GameObject character, Vector3 targetPlace){
+		//the char needs its Dragonbone Animation
+		if(waiting){
+			yield return move(character, targetPlace);
+		}
+		else{
+			StartCoroutine(move(character, targetPlace));
+			yield return null;
+		}
+	}
+	
+	public IEnumerator move(GameObject character, Vector3 targetPlace){
+		//the char needs its Dragonbone Animation
+		ani = character.GetComponent<UnityArmatureComponent>().armature.animation;
+		ani.Play("walk");
+		
+		while( Vector3.Distance(character.transform.localPosition, targetPlace) < 0.001f){
+			character.transform.localPosition = Vector3.MoveTowards(character.transform.localPosition, targetPlace, speed * Time.deltaTime);
+			yield return null;
+		}
+		ani.Play("stand");
+		yield return null;
+	}
+}
