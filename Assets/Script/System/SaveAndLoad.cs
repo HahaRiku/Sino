a0627bf5e1652c7de3bdce37e91139cd2b474fe3
HahaRiku <s9323839@gmail.com>
2019-05-25 03:14:40 +0900

    SaveAndLoad

---
+++ Assets/Script/System/SaveAndLoad.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[System.Serializable]
+public class SavedData {
+
+    public string scene;
+    public long playedTime;
+    public long systemTime;
+    public float playerX;
+    public string globalVariables;
+
+}
+
+public class SaveAndLoad {
+
+    public void Save(int id) {
+        string savedFileName = id.ToString();
+        string scene = SystemVariables.Scene;
+        long playedTime = SystemVariables.PlayedTime;
+        long SystemTime = (Int64)(DateTime.Now.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+        string GlobalVars = JsonUtility.ToJson(SystemVariables.otherVariables, false);
+        //float playerX= //I need Actor or something
+
+        SavedData SavedD = new SavedData {
+            scene = scene,
+            playedTime = playedTime,
+            systemTime = SystemTime,
+            //playerX
+            globalVariables = GlobalVars
+        };
+
+        string data = JsonUtility.ToJson(SavedD, false);
+    }
+
+}
