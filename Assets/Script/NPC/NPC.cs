8a681fa028cea7aab6a15b6abb7b33a1d99d4138
HahaRiku <s9323839@gmail.com>
2019-05-28 20:21:02 +0900

    [新增]NPC not done

--- Assets/Script/NPC/NPC.cs
+++ Assets/Script/NPC/NPC.cs
@@ -15,19 +15,24 @@ public class NPC : MonoBehaviour {
 
     public enum Type {
         item,
-        NPC,
+        talk,
         door
     }
 
     public State state = State.範圍外;
+    public Type type;
     public GameObject player;
+    public Sprite 放大鏡;
+    public Sprite 放大鏡_發光;
 
     private Actor actor;
+    private SpriteRenderer 放大鏡SP;
     private bool toggleWithPlayer= false;
 
 	// Use this for initialization
 	void Start () {
         actor = GameObject.Find("Actor").GetComponent<Actor>();
+        放大鏡SP = gameObject.transform.GetChild(0).GetComponent<SpriteRenderer>();
         player = actor.userCharacter;
 	}
 	
@@ -46,12 +51,28 @@ public class NPC : MonoBehaviour {
 
         if (state == State.範圍外 && toggleWithPlayer) {
             state = State.可以講話;
+            if (type != Type.talk) {
+                放大鏡SP.sprite = 放大鏡_發光;
+            }
+            else {
+
+            }
         }
         else if (state == State.可以講話 && Input.GetKeyDown(KeyCode.Z)) {
             state = State.對話中;
             SystemVariables.lockMoving = true;
+            //dialog
         }
+        else if (state != State.範圍外 && !toggleWithPlayer) {
+            state = State.範圍外;
+            if (type != Type.talk) {
+                放大鏡SP.sprite = 放大鏡;
+            }
+            else {
 
+            }
+        }
+        
 
 	}
 }
