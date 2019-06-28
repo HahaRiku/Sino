a2b3a8fd96d0964bac424bdf0e2c71aa78c1ddc5
kopa0506 <kopa860506@gmail.com>
2019-06-28 21:07:53 +0800

    [更新] 調整對話框動畫

--- Assets/Script/Dialog/DialogAnimation.cs
+++ Assets/Script/Dialog/DialogAnimation.cs
@@ -3,32 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class DialogAnimTest : MonoBehaviour
+public class DialogAnimation : MonoBehaviour
 {
 
     public AnimationCurve animationCurve;
     public CanvasGroup 對話框;
     public CanvasGroup 人物;
     // Use this for initialization
-    void Start()
-    {
 
+    public void StartOpenAnim()
+    {
+        StartCoroutine(OpenAnim());
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StartCloseAnim()
     {
-
-    }
-
-    public void Anim1()
-    {
-        StartCoroutine(anim1());
-    }
-
-    public void Anim2()
-    {
-        StartCoroutine(anim2());
+        StartCoroutine(CloseAnim());
     }
 
     IEnumerator anim1()
@@ -100,6 +90,49 @@ public class DialogAnimTest : MonoBehaviour
             y = animationCurve.Evaluate(timer / totalTime) * (finalY - orginY);
             Camera.main.transform.position = new Vector3(0, finalY - y, -10);
             Camera.main.orthographicSize = 5.5f + y;
+            if (timer < interval)
+            {
+                alpha = animationCurve.Evaluate(timer / interval);
+                對話框.alpha = 1 - alpha;
+                人物.alpha = 1 - alpha;
+            }
+            yield return new WaitForSeconds(Time.deltaTime);
+        }
+        Camera.main.transform.position = new Vector3(0, orginY, -10);
+        對話框.alpha = 0;
+        人物.alpha = 0;
+    }
+
+    IEnumerator OpenAnim()
+    {
+        float timer = 0, orginY = 0.0f, finalY = -0.5f, totalTime = 0.5f, interval = 0.2f, y, alpha;
+        float waitBackTime = 1.0f;
+        while (timer < totalTime)
+        {
+            timer += Time.deltaTime;
+            y = animationCurve.Evaluate(timer / totalTime) * (finalY - orginY);
+            Camera.main.transform.position = new Vector3(0, orginY + y, -10);
+            if (timer > totalTime - interval)
+            {
+                alpha = animationCurve.Evaluate((timer - totalTime + interval) / interval);
+                對話框.alpha = alpha;
+                人物.alpha = alpha;
+            }
+            yield return new WaitForSeconds(Time.deltaTime);
+        }
+        Camera.main.transform.position = new Vector3(0, finalY, -10);
+        對話框.alpha = 1;
+        人物.alpha = 1;
+    }
+
+    IEnumerator CloseAnim()
+    {
+        float timer = 0, orginY = 0.0f, finalY = -0.5f, totalTime = 0.5f, interval = 0.2f, y, alpha;
+        while (timer < totalTime)
+        {
+            timer += Time.deltaTime;
+            y = animationCurve.Evaluate(timer / totalTime) * (finalY - orginY);
+            Camera.main.transform.position = new Vector3(0, finalY - y, -10);
             if (timer < interval)
             {
                 alpha = animationCurve.Evaluate(timer / interval);
--- Assets/Script/StorySystem/DialogPanelController.cs
+++ Assets/Script/StorySystem/DialogPanelController.cs
@@ -12,16 +12,18 @@ public class DialogPanelController : MonoBehaviour
     }
     public void SetInvisible()
     {
-        GetComponent<CanvasGroup>().alpha = 0;
+        //GetComponent<CanvasGroup>().alpha = 0;
         GetComponent<CanvasGroup>().interactable = false;
         GetComponent<CanvasGroup>().blocksRaycasts = false;
+        GetComponent<DialogAnimation>().StartCloseAnim();
         _v = false;
     }
     public void SetVisible()
     {
-        GetComponent<CanvasGroup>().alpha = 1;
+        //GetComponent<CanvasGroup>().alpha = 1;
         GetComponent<CanvasGroup>().interactable = true;
         GetComponent<CanvasGroup>().blocksRaycasts = true;
+        GetComponent<DialogAnimation>().StartOpenAnim();
         _v = true;
     }
 }
