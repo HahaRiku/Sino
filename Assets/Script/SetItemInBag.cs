41bf0114b847940f6a358828f7e43119b468de45
HahaRiku <s9323839@gmail.com>
2019-07-06 11:37:18 +0900

    加糖果

---
+++ Assets/Script/SetItemInBag.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetItemInBag : MonoBehaviour {
+
+    public string ItemName;
+
+    public void SetInBag() {
+        BagSystem.SetItemInBagOrNot(ItemName, true);
+    }
+
+}
