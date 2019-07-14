f3a70902a245b66b102bec42c450d97ea81daff6
kopa0506 <kopa860506@gmail.com>
2019-07-15 01:03:09 +0800

    [更新] 席諾正背面動畫

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -6,6 +6,13 @@ using DragonBones;
 public class PlayerController : MonoBehaviour
 {
     private UnityArmatureComponent arma;
+    private UnityArmatureComponent arma2;
+
+    enum DisplayStatus
+    {
+        左右,
+        正背
+    }
     enum CollisionStatus
     {
         none,
@@ -13,7 +20,8 @@ public class PlayerController : MonoBehaviour
         right_collision
     }
     CollisionStatus nowCollisionStatus;
- 
+    DisplayStatus nowDisplayStatus;
+
     private bool isPlayerCanControl = false;
 
     public bool IsHoldingCandle = false;		//預設為不拿蠟燭，須從外面腳本改動
@@ -25,11 +33,16 @@ public class PlayerController : MonoBehaviour
 
     void Start() {
         arma = transform.GetChild(0).GetComponent<UnityArmatureComponent>();
+        arma2 = transform.GetChild(1).GetComponent<UnityArmatureComponent>();
+        transform.GetChild(0).gameObject.SetActive(false);
+        transform.GetChild(1).gameObject.SetActive(true);
+        nowDisplayStatus = DisplayStatus.正背;
         AnimationController("idle");
     }
 
     void Update () {
         if (isPlayerCanControl) {
+            Debug.Log(arma._armature);
             if (nowCollisionStatus == CollisionStatus.left_collision && Input.GetKey("left"))
             {
                 AnimationController("idle");
@@ -78,6 +91,17 @@ public class PlayerController : MonoBehaviour
                     flip = -1;
                 }
             }
+
+            if(Input.GetKeyDown("up"))
+            {
+                AnimationController("reverse_side");
+                flip = 0;
+            }
+            if (Input.GetKeyDown("down"))
+            {
+                AnimationController("front_side");
+                flip = 0;
+            }
             transform.Translate(flip * Vector2.right * speed * _v * Time.deltaTime);
             return;
         }
@@ -85,27 +109,46 @@ public class PlayerController : MonoBehaviour
 	
 	
 	public void AnimationController(string command){
-		switch(command){				
-			case "idle":
+        switch (command)
+        {
+            case "idle":
+                if (nowDisplayStatus != DisplayStatus.左右)
+                {
+                    transform.GetChild(0).gameObject.SetActive(true);
+                    transform.GetChild(1).gameObject.SetActive(false);
+                    nowDisplayStatus = DisplayStatus.左右;
+                }
                 if (arma.animationName == "stand")
                     break;
-                if(!IsHoldingCandle)
+                if (!IsHoldingCandle)
                     arma.animation.FadeIn("stand", 0.08f);
-				else if(arma.armature.flipX)
+                else if (arma.armature.flipX)
                     arma.animation.FadeIn("stand_with_candle_left", 0.03f);
                 else
                     arma.animation.FadeIn("stand_with_candle_right", 0.03f);
                 arma.animationName = "stand";
-                break;		
-			case "walk_right":
+                break;
+            case "walk_right":
+                if (nowDisplayStatus != DisplayStatus.左右)
+                {
+                    transform.GetChild(0).gameObject.SetActive(true);
+                    transform.GetChild(1).gameObject.SetActive(false);
+                    nowDisplayStatus = DisplayStatus.左右;
+                }
                 arma.armature.flipX = true;
                 if (!IsHoldingCandle)
                     arma.animation.FadeIn("walk", 0.1f);
                 else
                     arma.animation.FadeIn("walk_with_candle_left", -1);
                 arma.animationName = "walk";
-                break;			
-			case "walk_left":
+                break;
+            case "walk_left":
+                if (nowDisplayStatus != DisplayStatus.左右)
+                {
+                    transform.GetChild(0).gameObject.SetActive(true);
+                    transform.GetChild(1).gameObject.SetActive(false);
+                    nowDisplayStatus = DisplayStatus.左右;
+                }
                 arma.armature.flipX = false;
                 if (!IsHoldingCandle)
                     arma.animation.FadeIn("walk", 0.1f);
@@ -114,12 +157,40 @@ public class PlayerController : MonoBehaviour
                 arma.animationName = "walk";
                 break;
             case "backward":
+                if (nowDisplayStatus != DisplayStatus.左右)
+                {
+                    transform.GetChild(0).gameObject.SetActive(true);
+                    transform.GetChild(1).gameObject.SetActive(false);
+                    nowDisplayStatus = DisplayStatus.左右;
+                }
                 arma.animation.FadeIn("backward", 0.08f, 1);
                 arma.animationName = "backward";
                 break;
+            case "front_side":
+                if (nowDisplayStatus != DisplayStatus.正背)
+                {
+                    transform.GetChild(0).gameObject.SetActive(false);
+                    transform.GetChild(1).gameObject.SetActive(true);
+                    nowDisplayStatus = DisplayStatus.正背;
+                }
+                arma2.animation.FadeIn("front_side");
+                arma2.animationName = "front_side";
+                arma2.armatureName = "Sino(正反)";
+                break;
+            case "reverse_side":
+                if (nowDisplayStatus != DisplayStatus.正背)
+                {
+                    transform.GetChild(0).gameObject.SetActive(false);
+                    transform.GetChild(1).gameObject.SetActive(true);
+                    nowDisplayStatus = DisplayStatus.正背;
+                }
+                arma2.animation.FadeIn("reverse_side");
+                arma2.animationName = "reverse_side";
+                arma2.armatureName = "Sino(正反)";
+                break;
             default:
                 break;
-		}
+        }
 	}
 	
     public void SetIsPlayerCanControl(bool b)
