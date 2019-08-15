4e65d4f370082b5a2269241bb3510d6dff04c543
HahaRiku <s9323839@gmail.com>
2019-08-15 19:48:44 +0900

    [新增]blur的一些東西

---
+++ Assets/Script/Effects/WakeUpAnimation.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WakeUpAnimation : MonoBehaviour {
+
+    public float maxValue = 1.2f;
+    public float minValue = 0;
+    public float gapBetweenFrame = 0.008f;
+
+    public bool start = false;
+
+    private Image blurBase;
+
+	// Use this for initialization
+	void Start () {
+        blurBase = GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (start) {
+            start = false;
+            StartCoroutine(BlurAnimation());
+        }
+	}
+
+    IEnumerator BlurAnimation() {
+        for (float i = maxValue; i >= minValue; i -= gapBetweenFrame) {
+            blurBase.material.SetFloat("_Size", i);
+            yield return null;
+        }
+        blurBase.material.SetFloat("_Size", 0);
+    }
+
+    public void StartWakeUpAnimation() {
+        start = true;
+    }
+}
