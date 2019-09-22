95193430e816bf898c0f467bd2c047e98233fa6e
HahaRiku <s9323839@gmail.com>
2019-09-22 18:06:25 +0900

    [修復]ObjActiveController修復

--- Assets/Script/ObjActiveController.cs
+++ Assets/Script/ObjActiveController.cs
@@ -9,11 +9,6 @@ public class ObjActiveController : MonoBehaviour {
         道具是否在背包
     }
 
-    public enum Target {
-        GameObject,
-        Component
-    }
-
     [System.Serializable]
     public struct Element {
         public Condition 要使用哪種判斷;
@@ -21,40 +16,38 @@ public class ObjActiveController : MonoBehaviour {
         public int 如果上面的變數為多少才設置;
         public string 道具名稱;
         public bool 道具是否在背包;
-        public bool 要設定Active的是GameObject還是Component;
-        public GameObject 如果是Obj_要設定Active的Obj;
-        public GameObject 如果是Comp_要設定Active的Comp;
+        public GameObject 要設定Active的物件;
         public bool Active的值;
     }
 
     public List<Element> 檢查的序列 = new List<Element>();
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
         if (檢查的序列.Count != 0) {
             foreach (Element e in 檢查的序列) {
                 if (e.要使用哪種判斷 == Condition.變數) {
                     if (SystemVariables.IsIntVariableExisted(e.變數名)) {
                         if (SystemVariables.otherVariables_int[e.變數名] == e.如果上面的變數為多少才設置) {
-                            e.如果是Obj_要設定Active的Obj.SetActive(e.Active的值);
+                            e.要設定Active的物件.SetActive(e.Active的值);
                             檢查的序列.Remove(e);
                             return;
                         }
                     }
                 }
                 else if (e.要使用哪種判斷 == Condition.道具是否在背包) {
-                    if (BagSystem.IsItemInBag(e.道具名稱) == e.道具是否在背包) {
-                        e.如果是Obj_要設定Active的Obj.SetActive(e.Active的值);
+                    if (BagSystem.IsItemInBag(e.道具名稱)) {
+                        e.要設定Active的物件.SetActive(e.Active的值);
                         檢查的序列.Remove(e);
                         return;
                     }
                 }
             }
         }
-	}
+    }
 }
