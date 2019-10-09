49c9c688faf1b04a0d200acefd1313475d52fe33
HahaRiku <s9323839@gmail.com>
2019-10-09 20:15:49 +0900

    上次忘記commit的先commit

---
+++ Assets/Script/TestScript/testmove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testmove : MonoBehaviour {
+    public float speed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.RightArrow)) {
+            if(transform.localScale.x>0) {
+                transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+            }
+            transform.localPosition = new Vector3(transform.localPosition.x + speed, transform.localPosition.y, transform.localPosition.z);
+        }
+        else if(Input.GetKey(KeyCode.LeftArrow)) {
+            if (transform.localScale.x < 0) {
+                transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+            }
+            transform.localPosition = new Vector3(transform.localPosition.x - speed, transform.localPosition.y, transform.localPosition.z);
+        }
+	}
+}
