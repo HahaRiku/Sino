835b9ec185d8b6013145c9e20490dc1f7520a79a
Hannibal3345 <55790139+Hannibal3345@users.noreply.github.com>
2019-10-16 21:08:04 +0800

    BGM 功能完成
    Function使用介紹:
    Play1-播放該編號的音樂
    musicOut-花費輸入的秒數音量降到0
    musicIn-花費輸入的秒數音量升至1
    switchingEX-花費輸入的秒數音量降到0(在BGM 的 script 下方的 OutspentTime輸入)，停止目前撥放的音樂並撥放目前編號指定的音樂，再花費輸入的秒數音量升至1(在BGM 的 script 下方的 InspentTime輸入)

--- Assets/Script/StorySystem/OuterScripts/BGM.cs
+++ Assets/Script/StorySystem/OuterScripts/BGM.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AudioOutput : MonoBehaviour 
+public class BGM: MonoBehaviour 
 {
 	public AudioClip[] soundaffect;
 	AudioSource audioSource;
