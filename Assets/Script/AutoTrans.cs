4a48fec0fb463114bfcc313d6e636775135e8eff
WanoonBrite <105502049@cc.ncu.edu.tw>
2019-11-13 22:35:07 +0800

    傳送_freeX_2

--- Assets/Script/AutoTrans.cs
+++ Assets/Script/AutoTrans.cs
@@ -9,10 +9,15 @@ public class AutoTrans : MonoBehaviour {
 	private Object targetScene;
 	[SerializeField]
 	private string targetSceneName;
+	[SerializeField]
+	private float freeX;
 	public GameStateManager.SpawnPoint nextScenePos;    //0=null , 1=L , 2=R , 3=door , 4=stair , 5=smallDoor	
 
     void OnTriggerEnter2D(Collider2D other)
     {
+        if(nextScenePos == 7){
+                GameStateManager.targetPos[7].x = freeX;
+        }
         FindObjectOfType<GameStateManager>().黑幕轉場(targetSceneName, nextScenePos);
         //GameStateManager.Instance.黑幕轉場(targetScene.name, nextScenePos);
     }
