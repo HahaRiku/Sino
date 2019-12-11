d9735eff6dc01e3d7e09b41e3c6bd0ee365a03f6
HahaRiku <s9323839@gmail.com>
2019-12-12 00:04:59 +0800

    [新增]外部腳本-隨機值給變數

---
+++ Assets/Script/StorySystem/OuterScripts/RandomVariable.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomVariable : MonoBehaviour {
+    [System.Serializable]
+    public struct Parameter {
+        public string variableName;
+        public int randomMax_exclusive;
+        public int randomMin_inclusive;
+    }
+    public Parameter[] parameterList;
+
+    public void RandomNumber(int index) {
+        SystemVariables.AddIntVariable(parameterList[index].variableName,
+            Random.Range(parameterList[index].randomMin_inclusive, parameterList[index].randomMax_exclusive));
+    }
+}
